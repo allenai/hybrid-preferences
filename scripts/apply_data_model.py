731273ab61ccfad8f0a1c014699d3622ac383d11
ljvmiranda921 <ljvmiranda@gmail.com>
2024-08-12 10:46:39 -0700

    [wip] Update

--- scripts/apply_data_model.py
+++ scripts/apply_data_model.py
@@ -1,6 +1,16 @@
 import argparse
 from pathlib import Path
 
+import sys
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[logging.StreamHandler(sys.stdout)],
+    level=logging.INFO,
+)
+
 
 def get_args():
     # fmt: off
@@ -13,19 +23,22 @@ Then, it will output a JSONL file containing the following fields:
 - `features_used`: a comma-separated string of features used for this instance.
 
 You can select a number of features by passing arguments to the `--features` option.
-If None is passed, then all features will be computed.
+By default all features will be computed.
 """
 
-    parser = argparse.ArgumentParser(description=description)
+    parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument("--input_path", type=Path, required=True, help="Path to the JSONL file containing preferences.")
     parser.add_argument("--output_path", type=Path, required=True, help="Path to save the output JSONL file.")
     parser.add_argument("--features", nargs="*", default=None, help="Features to include. To show all available features, pass --show_all_features.")
+    parser.add_argument("--show_all_features", action="store_true", default=False, help="If set, will just show all available features and exit the CLI.")
     # fmt: on
     return parser.parse_args()
 
 
 def main():
-    pass
+    args = get_args()
+    if args.show_all_features:
+        logging.info("Hi")
 
 
 if __name__ == "__main__":
