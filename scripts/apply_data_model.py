f7edff67ef3a3d1d85cf96765d69854366c20897
Lj Miranda <ljvmiranda@gmail.com>
2024-08-13 20:29:09 -0700

    [wip] Update

--- scripts/apply_data_model.py
+++ scripts/apply_data_model.py
@@ -4,6 +4,7 @@ import logging
 import random
 import sys
 from pathlib import Path
+from typing import Optional
 
 import pandas as pd
 
@@ -19,7 +20,6 @@ logging.basicConfig(
 
 
 def get_args():
-    # fmt: off
     description = """Apply data model to get swapped preferences.
 
 This CLI expects a JSONL file with fields `pref_human` and `pref_gpt4`.  Then,
@@ -47,30 +47,93 @@ extract all features.
 
 """
 
+    # fmt: off
     parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument("--input_path", type=Path, required=False, help="Path to the JSONL file containing preferences.")
-    parser.add_argument("--output_dir", type=Path, required=False, help="Directory to save the output JSONL file.")
-    parser.add_argument("--num_instances", type=int, default=7000, help="Number of instances to save in the output file.")
-    parser.add_argument("--features", nargs="*", default=None, help="Features to include. To show all available features. If not set, will try all feature combinations. Pass --show_all_combinations to show all features.")
-    parser.add_argument("--threshold", type=float, default=1.0, help="Percentage of total features to be active in order to swap w/ human preferences.")
-    parser.add_argument("--keep_features_dir", type=Path, default=None, help="If set, will store all collected features in this directory.")
-    parser.add_argument("--append_to_experiments_file", type=Path, default=None, help="If set, will append to an experiments TXT file to be used for submitting TPU training jobs.")
-    parser.add_argument("--random_seed", type=int, default=42, help="Set the random seed.")
-    parser.add_argument("--show_all_features", action="store_true", help="Show all available features and exit.")
+    subparsers = parser.add_subparsers(dest="command")
+
+    # Define shared arguments
+    shared_args = argparse.ArgumentParser(add_help=False)
+    shared_args.add_argument("--input_path", type=Path, required=False, help="Path to the JSONL file containing preferences.")
+    shared_args.add_argument("--output_dir", type=Path, required=False, help="Directory to save the output JSONL file.")
+    shared_args.add_argument("--num_instances", type=int, default=7000, help="Number of instances to save in the output file.")
+    shared_args.add_argument("--features", nargs="*", default=None, help="Features to include. To show all available features. If not set, will try all feature combinations. Pass --show_all_combinations to show all features.")
+    shared_args.add_argument("--threshold", type=float, default=1.0, help="Percentage of total features to be active in order to swap w/ human preferences.")
+    shared_args.add_argument("--keep_features_dir", type=Path, default=None, help="If set, will store all collected features in this directory.")
+    shared_args.add_argument("--append_to_experiments_file", type=Path, default=None, help="If set, will append to an experiments TXT file to be used for submitting TPU training jobs.")
+    shared_args.add_argument("--random_seed", type=int, default=42, help="Set the random seed.")
+
+    # Arguments for 'single' command
+    parser_single = subparsers.add_parser("single", help="Run single data model run", parents=[shared_args])
+    parser_single.add_argument("--show_all_features", action="store_true", help="Show all available features and exit.")
+
+    # Arguments for 'multi' command
+    parser_multi = subparsers.add_parser("multi", help="Run multiple combinations", parents=[shared_args])
+    parser_multi.add_argument("--n_sample_combinations", type=int, default=None, help="Sample the combinations to run.")
+
     # fmt: on
     return parser.parse_args()
 
 
 def main():
     args = get_args()
+    all_features, all_combinations = get_all_feature_combinations()
 
-    all_features, _ = get_all_feature_combinations()
-    if args.show_all_features:
-        logging.info(f"Available features: {', '.join(all_features)}")
-        sys.exit(0)
+    if args.command == "single":
+        if args.show_all_features:
+            logging.info(f"Available features: {', '.join(all_features)}")
+            sys.exit(0)
 
-    random.seed(args.random_seed)
-    df = pd.read_json(args.input_path, lines=True)
+        apply_data_model(
+            input_path=args.input_path,
+            output_dir=args.output_dir,
+            keep_features_dir=args.keep_features_dir,
+            features=args.features,
+            threshold=args.threshold,
+            random_seed=args.random_seed,
+            num_instances=args.num_instances,
+            append_to_experiments_file=args.append_to_experiments_file,
+        )
+    elif args.command == "multi":
+
+        feats_to_run = (
+            random.sample(all_combinations, args.n_sample_combinations)
+            if args.n_sample_combinations
+            else all_combinations
+        )
+        logging.info(
+            f"There are {len(all_combinations)} total feature combinations."
+            f" Running {len(feats_to_run)}."
+        )
+
+        for idx, feature_combination in enumerate(feats_to_run):
+            logging.info(
+                f"*** Feature {idx+1}/{len(feats_to_run)}: {', '.join(feature_combination)} ***"
+            )
+
+            apply_data_model(
+                input_path=args.input_path,
+                output_dir=args.output_dir,
+                keep_features_dir=args.keep_features_dir,
+                features=feature_combination,
+                threshold=args.threshold,
+                random_seed=args.random_seed,
+                num_instances=args.num_instances,
+                append_to_experiments_file=args.append_to_experiments_file,
+            )
+
+
+def apply_data_model(
+    input_path: Path,
+    output_dir: Path,
+    keep_features_dir: Optional[Path] = None,
+    features: Optional[list[str]] = None,
+    threshold: float = 1.0,
+    random_seed: int = 42,
+    num_instances: int = 7000,
+    append_to_experiments_file: Optional[Path] = None,
+):
+    random.seed(random_seed)
+    df = pd.read_json(input_path, lines=True)
     if not {"pref_human", "pref_gpt4"}.issubset(set(list(df.columns))):
         logging.error("Columns 'pref_human' and 'pref_gpt4' should be present!")
         sys.exit(1)
@@ -83,15 +146,14 @@ def main():
         prompt_col="text",
         completion_a_col="response_a",
         completion_b_col="response_b",
-        keep_features=args.keep_features_dir,
+        keep_features=keep_features_dir,
     )
-    features = args.features
     if not features:
         logging.info(
             "Will extract all available features using their default parameters"
         )
         features = list(extractor.REGISTERED_EXTRACTORS.keys())
-    extracted_df = extractor(features=features, threshold=args.threshold)
+    extracted_df = extractor(features=features, threshold=threshold)
 
     # Convert to DPO training format
     logging.info("Converting annotations into DPO training format")
@@ -112,20 +174,21 @@ def main():
             converted_annotations.append(converted_instance)
     logging.info(f"Number of instances after selection: {len(converted_annotations)}")
 
-    if args.num_instances < len(converted_annotations):
-        converted_annotations = random.sample(converted_annotations, args.num_instances)
-        logging.info(f"Sampled {args.num_instances} instances from the total.")
+    # Sample converted instances
+    if num_instances < len(converted_annotations):
+        converted_annotations = random.sample(converted_annotations, num_instances)
+        logging.info(f"Sampled {num_instances} instances from the total.")
 
-    output_dir: Path = args.output_dir
     output_dir.mkdir(parents=True, exist_ok=True)
     tag = "___".join(features).replace("::", "__").replace("=", "-")
-    output_path = output_dir / f"human_datamodel_{args.num_instances}_FEATS_{tag}.jsonl"
+    output_path = output_dir / f"human_datamodel_{num_instances}_FEATS_{tag}.jsonl"
     with output_path.open("w") as f:
         for annotation in converted_annotations:
             f.write(json.dumps(annotation) + "\n")
     logging.info(f"Saved to {output_path}")
 
-    experiments_file: Path = args.append_to_experiments_file
+    # Create a TXT file so that it's easier to submit tpu jobs
+    experiments_file: Path = append_to_experiments_file
     if experiments_file:
         experiment_name = output_path.stem
         if experiments_file.exists():
