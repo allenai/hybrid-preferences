7143624795fe7f2ca277264e938542ba277ecedc
ljvmiranda921 <ljvmiranda@gmail.com>
2024-08-14 19:43:15 -0700

    Fix string formatting and concat

--- scripts/submit_tpu_train_job.py
+++ scripts/submit_tpu_train_job.py
@@ -11,67 +11,71 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-DPO_JOB_TEMPLATE = """
-python3 -m EasyLM.models.llama.llama_train_dpo \
---mesh_dim='1,-1,4' \
---dtype='bf16' \
---num_epochs=3 \
---log_freq=50 \
---save_model_freq=1000 \
---save_milestone_freq=0 \
---load_llama_config='13b' \
---update_llama_config='' \
---load_dataset_state='' \
---load_checkpoint='params::{ckpt_gcs_path}' \
---tokenizer.vocab_file='{vocab_gcs_path}' \
---optimizer.type='adamw' \
---optimizer.adamw_optimizer.weight_decay=0.0 \
---optimizer.adamw_optimizer.lr=5e-7 \
---optimizer.adamw_optimizer.end_lr=0 \
---optimizer.adamw_optimizer.warmup_ratio=0.1 \
---optimizer.accumulate_gradient_steps=4 \
---train_dataset.type='preference_json_torch' \
---train_dataset.json_torch_dataset.path='{input_gcs_path}/{experiment_name}.jsonl' \
---train_dataset.json_torch_dataset.seq_length=4096 \
---train_dataset.json_torch_dataset.batch_size=8 \
---checkpointer.save_optimizer_state=False \
---logger.online=True --logger.project='human-preferences' --logger.entity='rlhf-llm-dev' \
---logger.prefix_to_id=True --logger.prefix='tulu2_13b_dpo_{experiment_name}' \
---logger.output_dir='{output_gcs_path}/checkpoints/'"
-"""
+DPO_JOB_TEMPLATE = (
+    "python3 -m EasyLM.models.llama.llama_train_dpo "
+    "--mesh_dim='1,-1,4' "
+    "--dtype='bf16' "
+    "--num_epochs=3 "
+    "--log_freq=50 "
+    "--save_model_freq=1000 "
+    "--save_milestone_freq=0 "
+    "--load_llama_config='13b' "
+    "--update_llama_config='' "
+    "--load_dataset_state='' "
+    "--load_checkpoint='params::{ckpt_gcs_path}' "
+    "--tokenizer.vocab_file='{vocab_gcs_path}' "
+    "--optimizer.type='adamw' "
+    "--optimizer.adamw_optimizer.weight_decay=0.0 "
+    "--optimizer.adamw_optimizer.lr=5e-7 "
+    "--optimizer.adamw_optimizer.end_lr=0 "
+    "--optimizer.adamw_optimizer.warmup_ratio=0.1 "
+    "--optimizer.accumulate_gradient_steps=4 "
+    "--train_dataset.type='preference_json_torch' "
+    "--train_dataset.json_torch_dataset.path='{input_gcs_path}/{experiment_name}.jsonl' "
+    "--train_dataset.json_torch_dataset.seq_length=4096 "
+    "--train_dataset.json_torch_dataset.batch_size=8 "
+    "--checkpointer.save_optimizer_state=False "
+    "--logger.online=True "
+    "--logger.project='human-preferences' "
+    "--logger.entity='rlhf-llm-dev' "
+    "--logger.prefix_to_id=True "
+    "--logger.prefix='tulu2_13b_dpo_{experiment_name}' "
+    "--logger.output_dir='{output_gcs_path}/checkpoints/'"
+)
 
-RM_JOB_TEMPLATE = """
-python3 -m EasyLM.models.llama.llama_train_rm \
---mesh_dim=1,-1,8 \
---dtype=bf16 \
---num_epochs=1 \
---log_freq=50 \
---save_model_freq=1000 \
---save_milestone_freq=0 \
---load_llama_config=13b \
---update_llama_config='' \
---load_dataset_state='' \
---load_checkpoint='params::{ckpt_gcs_path}' \
---tokenizer.vocab_file='{vocab_gcs_path}' \
---optimizer.type=adamw \
---optimizer.adamw_optimizer.weight_decay=0.0 \
---optimizer.adamw_optimizer.lr=1e-5 \
---optimizer.adamw_optimizer.end_lr=1e-6 \
---optimizer.adamw_optimizer.warmup_ratio=0.03 \
---optimizer.accumulate_gradient_steps=4 \
---train_dataset.type=preference_json_torch \
---train_dataset.json_torch_dataset.path='{input_gcs_path}/{experiment_name}.jsonl' \
---train_dataset.json_torch_dataset.seq_length=4096 \
---train_dataset.json_torch_dataset.batch_size=16 \
---checkpointer.save_optimizer_state=False \
---train_dataset.json_torch_dataset.remove_truncated_samples=True \
---logger.online=True \
---logger.project=human_preferences_rm \
---logger.entity=rlhf-llm-dev \
---logger.prefix_to_id=True \
---logger.prefix=tulu2_13b_rm_{experiment_name} \
---logger.output_dir='{output_gcs_path}/rm_checkpoints/'
-"""
+
+RM_JOB_TEMPLATE = (
+    "python3 -m EasyLM.models.llama.llama_train_rm "
+    "--mesh_dim=1,-1,8 "
+    "--dtype=bf16 "
+    "--num_epochs=1 "
+    "--log_freq=50 "
+    "--save_model_freq=1000 "
+    "--save_milestone_freq=0 "
+    "--load_llama_config=13b "
+    "--update_llama_config='' "
+    "--load_dataset_state='' "
+    "--load_checkpoint='params::{ckpt_gcs_path}' "
+    "--tokenizer.vocab_file='{vocab_gcs_path}' "
+    "--optimizer.type=adamw "
+    "--optimizer.adamw_optimizer.weight_decay=0.0 "
+    "--optimizer.adamw_optimizer.lr=1e-5 "
+    "--optimizer.adamw_optimizer.end_lr=1e-6 "
+    "--optimizer.adamw_optimizer.warmup_ratio=0.03 "
+    "--optimizer.accumulate_gradient_steps=4 "
+    "--train_dataset.type=preference_json_torch "
+    "--train_dataset.json_torch_dataset.path='{input_gcs_path}/{experiment_name}.jsonl' "
+    "--train_dataset.json_torch_dataset.seq_length=4096 "
+    "--train_dataset.json_torch_dataset.batch_size=16 "
+    "--checkpointer.save_optimizer_state=False "
+    "--train_dataset.json_torch_dataset.remove_truncated_samples=True "
+    "--logger.online=True "
+    "--logger.project=human_preferences_rm "
+    "--logger.entity=rlhf-llm-dev "
+    "--logger.prefix_to_id=True "
+    "--logger.prefix=tulu2_13b_rm_{experiment_name} "
+    "--logger.output_dir='{output_gcs_path}/rm_checkpoints/'"
+)
 
 
 def get_args():
