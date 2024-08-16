8a6b93fc9a06b7c0adaefdf97deb2e0107809847
ljvmiranda921 <ljvmiranda@gmail.com>
2024-08-16 11:47:44 -0700

    [wip] Update

--- scripts/convert_to_hf.py
+++ scripts/convert_to_hf.py
@@ -3,14 +3,15 @@
 Usage:
 
 ```
-pip install google-cloud-storage beaker-py
-gcloud auth application-default login
-gcloud auth application login
+# Setup EasyLM
 git clone https://github.com/hamishivi/EasyLM.git
 cd EasyLM
 gsutil cp gs://hamishi-east1/easylm/llama/tokenizer.model .
 conda env create -f scripts/gpu_environment.yml
 conda activate EasyLM
+pip install google-cloud-storage beaker-py
+gcloud auth application-default login
+gcloud auth application login
 # Copy this script into the machine you're working on
 python convert_to_hf.py --gcs_bucket <BUCKET_NAME> --gcs_dir_path <PREFIX> --parent_dir <OUTPUT>
 ```
@@ -22,6 +23,7 @@ import argparse
 import subprocess
 import logging
 from pathlib import Path
+from typing import Any
 
 try:
     from google.cloud import storage
@@ -43,8 +45,9 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--gcs_bucket", type=str, help="GCS bucket where the models are stored (NO need for gs:// prefix).")
     parser.add_argument("--gcs_dir_path", type=str, help="The directory path (or prefix) of models (e.g., human-preferences/rm_checkpoints/tulu2_13b_rm_human_datamodel_).")
-    parser.add_argument("--parent_dir", type=Path, help="Directory where all downloads and outputs will be stored.")
+    parser.add_argument("--download_dir", type=Path, help="Directory where all parameter downloads from GCS will be stored. Will be emptied for every batch.")
     parser.add_argument("--tokenizer_path", type=str, default="tokenizer.model", help="Path where you downloaded the tokenizer model.")
+    parser.add_argument("--batch_size", type=int, default=5, help="Number of models to download before deleting.")
     # fmt: on
     return parser.parse_args()
 
@@ -60,34 +63,25 @@ def main():
     )
     logging.info(f"Found {len(params_gcs_paths)} parameter files.")
 
-    params_dict = {}
-    for idx, gcs_path in enumerate(params_gcs_paths):
-        # Create experiment name as an ID
-        experiment_name = Path(gcs_path.name).parent.stem.split("--")[0]
-        logging.info(
-            f"*** Downloading {experiment_name} ({idx+1} / {len(params_gcs_paths)}) ***"
-        )
-        # Input and output directories
-        param_dir = parent_dir / experiment_name
-        out_dir = parent_dir / f"{experiment_name}_OUT"
-        param_dir.mkdir(parents=True, exist_ok=True)
-        out_dir.mkdir(parents=True, exist_ok=True)
-        subprocess.run(
-            [
-                "gsutil",
-                "-m",
-                "cp",
-                f"gs://{args.gcs_bucket}/{gcs_path.name}/",
-                str(param_dir),
-            ],
-            check=True,
-        )
-        # Create output file and save outputs there
-        # param_file = "streaming_params"
-        # download_path = param_dir / param_file
-        # gcs_path.chunk_size = 4 * 1024 * 1024
-        # gcs_path.download_to_filename(str(download_path))
-        # params_dict[str(download_path)] = out_dir
+    src_files = [gcs_path.name for gcs_path in params_gcs_paths]
+    batches = make_batch(src_files, batch_size=args.batch_size)
+    logging.info(f"Converting into batches of {args.batch_size} to save space")
+    for idx, batch in enumerate(batches):
+        logging.info(f"*** Processing batch: {idx+1} ***")
+        with open("src_files.txt", "w") as f:
+            for line in batch:
+                filedir = Path(line).parent
+                f.write(f"gs://{args.gcs_bucket}/{filedir}\n")
+
+        download_command = f"cat src_files.txt | gsutil -m cp -I -r {parent_dir}"
+        subprocess.run(download_command, text=True, shell=True, capture_output=False)
+
+    # Create output file and save outputs there
+    # param_file = "streaming_params"
+    # download_path = param_dir / param_file
+    # gcs_path.chunk_size = 4 * 1024 * 1024
+    # gcs_path.download_to_filename(str(download_path))
+    # params_dict[str(download_path)] = out_dir
 
     # for idx, (input_params, output_dir) in enumerate(params_dict.items()):
     #     logging.info(f"Converting {download_path} to HF format")
@@ -101,6 +95,10 @@ def main():
     #         f"--output_dir={output_dir}",
     #     ]
     #     subprocess.run(convert_command, check=True)
+
+
+def make_batch(l: list[Any], batch_size: int) -> list[list[Any]]:
+    return [l[i : i + batch_size] for i in range(0, len(l), batch_size)]
 
 
 def list_directories_with_prefix(
