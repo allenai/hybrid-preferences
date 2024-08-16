3b2b2a5ad7b582d6b3af8be414d5c05a50ae934c
ljvmiranda921 <ljvmiranda@gmail.com>
2024-08-16 14:26:16 -0700

    [wip] Update

--- scripts/convert_to_hf.py
+++ scripts/convert_to_hf.py
@@ -6,10 +6,11 @@ Usage:
 # Setup EasyLM
 git clone https://github.com/hamishivi/EasyLM.git
 cd EasyLM
-gcloud auth login
-gsutil cp gs://hamishi-east1/easylm/llama/tokenizer.model .
+git checkout bc241782b67bbe926e148ec9d2046d76b7ba58c8 .
 conda env create -f scripts/gpu_environment.yml
 conda activate EasyLM
+gcloud auth login
+gsutil cp gs://hamishi-east1/easylm/llama/tokenizer.model .
 pip install google-cloud-storage beaker-py
 gcloud auth application-default login
 # Copy this script into the machine you're working on
