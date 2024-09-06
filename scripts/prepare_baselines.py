a39efed1952e17a7acc910023fd84882b98d3f20
ljvmiranda921 <ljvmiranda@gmail.com>
2024-09-06 10:51:55 -0700

    [wip] Update

--- scripts/prepare_baselines.py
+++ scripts/prepare_baselines.py
@@ -1,12 +1,19 @@
 import argparse
+from pathlib import Path
 
 
 def get_args():
-    pass
+    # fmt: off
+    description = "Get baseline datasets and their respective experiments.txt file"
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument("--output_dir", type=Path, help="Directory to save the JSONL files and the TXT experiments file.")
+    parser.add_argument("--input_filepath", type=Path, help="Dataset path to create baselines on.")
+    # fmt: on
+    return parser.parse_args()
 
 
 def main():
-    pass
+    args = get_args()
 
 
 if __name__ == "__main__":
