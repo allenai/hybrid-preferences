1dae5f4f53d121b0a92a8f95f229a7621904945b
ljvmiranda921 <ljvmiranda@gmail.com>
2024-09-06 10:53:47 -0700

    [wip] Update

--- scripts/prepare_baselines.py
+++ scripts/prepare_baselines.py
@@ -16,5 +16,52 @@ def main():
     args = get_args()
 
 
+def convert_to_dpo_format(
+    instance: dict[str, str], preference_label: str
+) -> dict[str, str]:
+    conversation_a = [
+        {"content": instance["prompt"], "role": "user"},
+        {"content": instance["completion_a"], "role": "assistant"},
+    ]
+    conversation_b = [
+        {"content": instance["prompt"], "role": "user"},
+        {"content": instance["completion_b"], "role": "assistant"},
+    ]
+    if preference_label.lower() in [
+        "a-is-slightly-better",
+        "a-is-clearly-better",
+        "a-is-better",
+    ]:
+        chosen = conversation_a
+        chosen_model = instance["model_a"]
+        rejected = conversation_b
+        rejected_model = instance["model_b"]
+    elif preference_label.lower() in [
+        "b-is-slightly-better",
+        "b-is-clearly-better",
+        "b-is-better",
+    ]:
+        chosen = conversation_b
+        chosen_model = instance["model_b"]
+        rejected = conversation_a
+        rejected_model = instance["model_a"]
+    elif preference_label.lower() == "tie":
+        return None
+    else:
+        raise ValueError(f"Invalid preference label: {preference_label}")
+    return {
+        "id": instance["id"],
+        "source": instance["source"],
+        "highest_level_degree": instance["highest_level_degree"],
+        "prompt": instance["prompt"],
+        "chosen": chosen,
+        "chosen_model": chosen_model,
+        "rejected": rejected,
+        "rejected_model": rejected_model,
+        "features_used": instance.get("features_used"),
+        "is_swapped": instance.get("is_swapped"),
+    }
+
+
 if __name__ == "__main__":
     main()
