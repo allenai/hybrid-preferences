f3b9521eb5bb2c9b82a95723a6decdab5e6c0aad
ljvmiranda921 <ljvmiranda@gmail.com>
2024-09-13 19:50:14 -0700

    [wip] Update

---
+++ scripts/sample_best_subset.py
@@ -0,0 +1,17 @@
+import random
+import argparse
+
+
+def get_args():
+    # fmt: off
+    description = "Select the best instances to swap to human annotations given a budget."
+    parser = argparse.ArgumentParser(description=description)
+    # fmt: on
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
