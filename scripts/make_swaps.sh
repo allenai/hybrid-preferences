322c4da3435f030bf5ef03f924b69fd99aa27317
ljvmiranda921 <ljvmiranda@gmail.com>
2024-09-19 13:26:37 -0700

    [wip] Update

--- scripts/make_swaps.sh
+++ scripts/make_swaps.sh
@@ -3,7 +3,7 @@ model=$2
 
 python3 -m scripts.sample_best_subset \
     --input_path data/helpsteer2_all_features/features.jsonl \
-    --output_dir data/helpsteer2_best_mixes_simulated_$model/ \
+    --output_dir data/helpsteer2_best_mixes_${sampling}_${model}/ \
     --model_path data/helpsteer2_${model}_model/model.pkl \
     --budget 0.25 0.50 0.75 \
     --response_a_col response_a --response_b_col response_b \
@@ -12,7 +12,7 @@ python3 -m scripts.sample_best_subset \
 
 python3 -m scripts.sample_best_subset \
     --input_path data/alpacafarm_all_features/features.jsonl \
-    --output_dir data/alpacafarm_best_mixes_simulated_$model/ \
+    --output_dir data/alpacafarm_best_mixes_${sampling}_${model}/ \
     --model_path data/helpsteer2_${model}_model/model.pkl \
     --budget 0.25 0.50 0.75 \
     --response_a_col response_a --response_b_col response_b \
@@ -21,7 +21,7 @@ python3 -m scripts.sample_best_subset \
 
 python3 -m scripts.sample_best_subset \
     --input_path data/multipref_all_features/features.jsonl \
-    --output_dir data/multipref_best_mixes_simulated_$model/ \
+    --output_dir data/multipref_best_mixes_${sampling}_${model}/ \
     --model_path data/helpsteer2_${model}_model/model.pkl \
     --budget 0.25 0.50 0.75 \
     --response_a_col response_a --response_b_col response_b \
@@ -29,7 +29,7 @@ python3 -m scripts.sample_best_subset \
 
 python3 -m scripts.sample_best_subset \
     --input_path data/chatarena_all_features/features.jsonl \
-    --output_dir data/chatarena_best_mixes_simulated_$model/ \
+    --output_dir data/chatarena_best_mixes_${sampling}_${model}/ \
     --model_path data/helpsteer2_${model}_model/model.pkl \
     --budget 0.25 0.50 0.75 \
     --response_a_col response_a --response_b_col response_b \
