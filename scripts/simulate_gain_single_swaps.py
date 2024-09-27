e068e9b5058ad6893cf6c1a0b9a53c210ed10703
ljvmiranda921 <ljvmiranda@gmail.com>
2024-09-26 17:05:04 -0700

    [wip] Update

--- scripts/simulate_gain_single_swaps.py
+++ scripts/simulate_gain_single_swaps.py
@@ -9,6 +9,8 @@ import pandas as pd
 import joblib
 
 from scripts.get_count_feats import get_instances, get_all_features
+from sklearn.preprocessing import QuantileTransformer
+
 
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
@@ -22,8 +24,6 @@ def get_args():
     # fmt: off
     description = "Simulate a dataset using a quadratic regressor and get the gain."
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("--input_path", type=Path, required=True, help="Path to the features.jsonl file for a given dataset.")
-    parser.add_argument("--feature_name", type=str, required=True, help="Feature name to simulate")
     parser.add_argument("--model_path", type=Path, required=True, help="Path to the model PKL file."),
     # fmt: on
     return parser.parse_args()
@@ -31,13 +31,25 @@ def get_args():
 
 def main():
     args = get_args()
-    input_df = pd.read_json(args.input_path, lines=True)
     model = joblib.load(args.model_path)
     feat_ext = (
         joblib.load(args.model_path.parent / "poly.pkl")
         if "quadratic" in str(args.model_path)
         else None
     )
+    features = get_all_features()
+    n = len(features)
+    df = pd.DataFrame(np.vstack([np.zeros(n), np.eye(n, dtype=int)]), columns=features)
+    df["pred"] = model.predict(feat_ext.transform(df))
+    df["gain"] = df["pred"] - df.loc[0, "pred"]
+    df["feature"] = df.iloc[:, :-2].idxmax(axis=1)
+    df["feature"].iloc[0] = "Pure synthetic annotations"
+
+    gain_df = df[["feature", "pred", "gain"]].sort_values(by="gain", ascending=False)
+    gain_df["gain_log"] = np.log1p(gain_df["gain"] * 1e5)
+    gain_df = gain_df[["feature", "gain_log"]].rename(columns={"gain_log": "gain"})
+
+    breakpoint()
 
 
 if __name__ == "__main__":
