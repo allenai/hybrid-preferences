01bedc5856600980fe527f7dfbe69d3328424323
ljvmiranda921 <ljvmiranda@gmail.com>
2024-09-27 20:06:11 -0700

    [wip] Update

--- scripts/simulate.py
+++ scripts/simulate.py
@@ -25,9 +25,10 @@ def get_args():
     description = "Simulate a dataset using a trained regressor and get the gain."
     parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument("--input_path", type=Path, required=True, help="Path to the features.jsonl file for a given dataset.")
-    parser.add_argument("--output_path", type=Path, required=True, help="Path to save the output in a CSV file."),
+    parser.add_argument("--output_dir", type=Path, required=True, help="Directory to save the output in a CSV file."),
     parser.add_argument("--model_path", type=Path, required=True, help="Path to the model PKL file."),
     parser.add_argument("--n_trials", type=int, default=3, help="Number of trials to run the simulator.")
+    parser.add_argument("--print_latex", action="store_true", default=False, help="Print LaTeX table.")
     parser.add_argument("--sim_type", choices=["dim_only", "actual"])
     # fmt: on
     return parser.parse_args()
@@ -37,21 +38,39 @@ def main():
     args = get_args()
     model, feat_ext = load_model(args.model_path)
     features = get_all_features()
-    n = len(features)
-    df = pd.DataFrame(np.vstack([np.zeros(n), np.eye(n, dtype=int)]), columns=features)
-    df["pred"] = model.predict(feat_ext.transform(df))
-    df["gain"] = df["pred"] - df.loc[0, "pred"]
-    df["feature"] = df.iloc[:, :-2].idxmax(axis=1)
-    df["feature"].iloc[0] = "Pure synthetic annotations"
 
-    gain_df = df[["feature", "pred", "gain"]].sort_values(by="gain", ascending=False)
-    gain_df["gain_log"] = np.log1p(gain_df["gain"] * 1e5)
-    gain_df = gain_df[["feature", "gain_log"]].rename(columns={"gain_log": "gain"})
+    output_dir = Path(args.output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
 
-    gain_df["feature"] = gain_df["feature"].apply(lambda x: fmt_prettyname(x))
-    gain_df = gain_df.reset_index(drop=True)
-    print(gain_df.to_latex(index=False))
-    gain_df.to_csv(args.output_path, index=False)
+    for random_swaps in [0, 0.25, 0.5, 0.75, 1.0]:
+        n = len(features)
+
+        baseline_vector = (
+            np.zeros(n)
+            if random_swaps == 0
+            else get_baseline(args.input_path, random_swaps)
+        )
+
+        df = pd.DataFrame(
+            np.vstack([baseline_vector, np.eye(n, dtype=int)]),
+            columns=features,
+        )
+        df["pred"] = model.predict(feat_ext.transform(df))
+        df["gain"] = df["pred"] - df.loc[0, "pred"]
+        df["feature"] = df.iloc[:, :-2].idxmax(axis=1)
+        df["feature"].iloc[0] = f"BASELINE_{random_swaps}"
+
+        gain_df = df[["feature", "pred", "gain"]].sort_values(
+            by="gain", ascending=False
+        )
+        gain_df["gain_log"] = np.log1p(gain_df["gain"] * 1e5)
+        gain_df = gain_df[["feature", "gain_log"]].rename(columns={"gain_log": "gain"})
+
+        gain_df["feature"] = gain_df["feature"].apply(lambda x: fmt_prettyname(x))
+        gain_df = gain_df.reset_index(drop=True)
+        if args.print_latex:
+            print(gain_df.to_latex(index=False))
+        gain_df.to_csv(output_dir / f"simulated_{random_swaps}.csv", index=False)
 
 
 def fmt_prettyname(feature_str: str) -> str:
